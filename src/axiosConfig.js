7038871d414a4b62f5c29454130180d88b53ee87
abulhasan-cct <110917677+abulhasan-cct@users.noreply.github.com>
2024-03-14 15:35:26 +0400

    Delete src directory

--- src/axiosConfig.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-//import { config } from 'dotenv';
-export const backEndDomain = process.env.REACT_APP_BACKEND_URL;
-console.log(backEndDomain);
-const instance = axios.create({
-  baseURL: backEndDomain,
-});
-
-export default instance;
